ec57d251cad995d914735735b82837b4c77adaf4
bilisumafromDZ <bilisuma.ambisa>
2022-04-08 12:24:55 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,14 +7,13 @@
  *
  * Return: Always 0 (Success)
  */
-/*
 int main(void)
 {
 int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n > 0)
+if (n > 0)
 printf("% is positive\n", n);
 else if (n < 0)
 printf("% is negative\n", n);
