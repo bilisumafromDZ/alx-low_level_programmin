b921fdb6e8d405a0f6ec0a21c360e6741a1336eb
bilisumafromDZ <bilisuma.ambisa>
2022-04-08 12:28:42 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,12 +14,12 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("% is positive\n", n);
+printf("%d is positive\n", n);
 else if (n < 0)
-printf("% is negative\n", n);
+printf("%d is negative\n", n);
 else
 
-printf("% is negative\n", n);
+printf("%d is negative\n", n);
 
 return (0);
 }
