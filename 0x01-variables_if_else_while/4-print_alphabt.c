9bb5e40a26986ea5d8be53849eaf310e51be90ac
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-04-09 04:41:43 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	
+	char l;
+	l = 'a';
+
+	while (l <= 'z')
+	{
+		if( l =! q && l =! e)
+		{
+			putchar(l);
+			putchar("\n");
+			l++
+		}
+	}
+	return (0);
+}
+
