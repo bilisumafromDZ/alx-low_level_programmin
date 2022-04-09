ce46b61780d0611bda2a0f5911e2fe20b50b6303
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-04-08 19:06:01 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	int c, i;
+int c, i;
 
-	for (c = '0'; c <= '9'; c++)
+        for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
@@ -30,6 +30,4 @@ int main(void)
 putchar('\n');
 
 return (0);
-}	
-
-
+}
