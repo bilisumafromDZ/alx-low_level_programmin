0443a7a814294f6c9cc727f8079041f0d3822ecb
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-04-12 13:16:16 -0700

    task 3

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,18 +7,22 @@
  */
 int main(void)
 {
-	int c, c_two;
+	int c, c_two, c_three;
 
-	for (c = 48; c <= 56; c++)
+	for (c = 48; c <= 55; c++)
 	{
-		for (c_two = c + 1; c_two <= 57; c_two++)
+		for (c_two = c + 1; c_two <= 56; c_two++)
 		{
-			putchar(c);
-			putchar(c_two);
-			if (c != 56 || c_two != 57)
+			for (c_three = c_two + 1; c_three <= 57; c_three++)
 			{
-				putchar(44);
-				putchar(32);
+				putchar(c);
+				putchar(c_two);
+				putchar(c_three);
+				if (c != 55 || c_two != 56 || c_three != 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
 			}
 		}
 	}
