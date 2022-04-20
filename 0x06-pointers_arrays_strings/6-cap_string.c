2879a13355ea16ec8a279548e4c3838fbc42b1fe
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-04-20 11:47:42 -0700

    test 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,24 +9,17 @@
  */
 char *cap_string(char *s)
 {
-	int a = 0, i;
-	int cspc = 13;
-	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
-
-	while (s[a])
+	int index = 0;
+	
+	while (s[index])
 	{
-		i = 0;
+		while (!(s[index] >= 'a' && s[index] <= 'z'))
+			index++;
 
-		while (i < cspc)
-		{
-			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-				s[a] -= 32;
-			
-			i++;
-			
-		}
-		
-		a++;
+		if (s[index - 1] == ' ' || s[index - 1] == '\t' || s[index - 1] == '\n' || s[index - 1] == ',' || s[index - 1] == ';' || s[index - 1] == '.' || s[index - 1] == '!' || s[index - 1] == '?' || s[index - 1] == '"' || s[index - 1] == '(' || s[index - 1] == ')' || s[index - 1] == '{' || s[index - 1] == '}' || index == 0)
+			s[index] -= 32;
+
+		index++;
 	}
 	
 	return (s);
