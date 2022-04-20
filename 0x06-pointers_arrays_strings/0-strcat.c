adc2c75542ef4cda414f902dfb34cbe9935e971e
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-04-20 10:46:03 -0700

    test 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,20 +10,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int dlen, i;
+	int index = 0, dest_len = 0;
 
-	dlen = 0;
-	while (dest[dlen])
-	{
-		dlen++;
-	}
-	
-	for (i = 0; src[i] != 0; i++)
-	{
-		dest[dlen] = src[i];
-		dlen++;
-	}
-	
-	dest[dlen] = '\0';
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
