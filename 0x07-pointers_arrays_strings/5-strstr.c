c92192afc47d13b921977d4e1a8f917caddf54c1
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-04-25 12:18:37 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,18 +12,18 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int index;
-
+	
 	if (*needle == 0)
 		return (haystack);
-
+	
 	while (*haystack)
 	{
 		index = 0;
-
+		
 		if (haystack[index] == needle[index])
 		{
-			do
-			{
+			do{
+				
 				if (needle[index + 1] == '\0')
 					return (haystack);
 
