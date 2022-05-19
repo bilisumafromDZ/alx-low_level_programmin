a1a2cfca5c07905f3007fdb77224d07a9e9d53db
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-05-19 05:42:48 -0700

    task all

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,11 +20,10 @@ typedef struct list_s
 } list_t;
 
 int _putchar(char);
-size_t print_list(const list_t *);
-size_t list_len(const list_t *);
-list_t *add_node(list_t **, const char *);
-list_t *add_node_end(list_t **, const char *);
-void print_start(void) __attribute__ ((constructor));
-void free_list(list_t *);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif /* LISTS */
