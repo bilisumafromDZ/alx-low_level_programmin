a479e852a0b3eb365deefc55af230c3489bc532c
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-05-19 06:08:12 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,14 +12,18 @@
 
 size_t print_list(const list_t *h)
 {
-	if (h == NULL)
-		return (0);
+	size_t count = 0;
 
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
-	else
-		printf("[%d] %s\n", h->len, h->str);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
+		h = h->next;
+		count++;
+	}
 
-		return (1 + print_list(h->next));
+	return (count);
 }
