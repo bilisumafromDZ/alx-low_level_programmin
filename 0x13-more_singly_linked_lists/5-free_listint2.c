d5d799e9279d40c82e536c4c341dd9d40fe610a8
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-05-23 11:45:21 -0700

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,15 +14,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *actual_node;
 	listint_t *next_node;
-	
+
 	actual_node = *head;
 
 	while (actual_node != NULL)
 	{
 		next_node = actual_node->next;
 		actual_node->next = NULL;
-		free(actual_node);	
-		actual_node = next_node;				
+		free(actual_node);
+		actual_node = next_node;
 	}
 	*head = NULL;
 }
