4344a14e0fc0685a3935b02d83b56db1bd7ac2bf
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-05-23 11:16:00 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,14 +11,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
+	unsigned int count = 0;
 
-	if (h != NULL)
+	while (h != NULL)
 	{
-		while (h)
-		{
-			printf("%d\n", h->n);													h = h->next;														count++;
-		}
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
 	}
+
 	return (count);
 }
