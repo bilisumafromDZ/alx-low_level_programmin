26d0b96df97ce06078cd1e96d81498424402e499
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-05-23 11:04:56 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -44,3 +44,31 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	return (tmp);
 }
+
+/**
+ * add_nodeint - adds a new node at the beginning of a list_t list.
+ * @head: Pointer to the actual position of the linked list (struct)
+ * @n: Value to include in the new node
+ *
+ * Return: Size of the linked_list.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
+
